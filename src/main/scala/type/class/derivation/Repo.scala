dfa3aadfc9ddf98e11438b6afcffb297e944191e
robertbutacu <robertbutacu@gmail.com>
2018-12-16 18:02:08 +0000

    added case classes

---
+++ src/main/scala/type/class/derivation/Repo.scala
@@ -0,0 +1,7 @@
+package `type`.`class`.derivation
+
+case class Repo(name: String, author: String, stars: Int)
+
+object Repo {
+
+}
---
+++ src/main/scala/type/class/derivation/YoutubeVideo.scala
@@ -0,0 +1,7 @@
+package `type`.`class`.derivation
+
+case class YoutubeVideo(name: String, publisher: String, likes: Int)
+
+object YoutubeVideo {
+
+}
