455b41b60efe55bf669057fed86287699d24e9fe
robertbutacu <robertbutacu@gmail.com>
2018-12-16 20:59:01 +0000

    comment

--- src/main/scala/typeclass/derivation/MyADT.scala
+++ src/main/scala/typeclass/derivation/MyADT.scala
@@ -3,4 +3,5 @@ package typeclass.derivation
 sealed trait MyADT
 
 case class A(x: String) extends MyADT
+//for recursiveness, see how the automatic type class derivation handles this
 case class B(anotherADT: MyADT, y: String) extends MyADT
